01f6d2ce953c5313f4aeac409fb0c62faef84063
degrandelucas <lucasdegrande15@gmail.com>
2024-09-14 23:07:50 -0300

    Operacoes Basicas com Lista em um Carrinho de Compra

--- List/OperacoesBasicas/src/CarrinhoDeCompras.java
+++ List/OperacoesBasicas/src/CarrinhoDeCompras.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarrinhoDeCompras {
 
+    private List<Item> itens;
+
+    //construtor
+    public CarrinhoDeCompras() {
+        this.itens = new ArrayList<>();
+    }
+    
+    //metodos
+    public void adicionarItem(String nome, double preco, int quantidade){
+        Item novoItem = new Item(nome, preco, quantidade);
+        itens.add(novoItem);
+    }
+
+    public void removerItemPorNome(String nome){
+        if (!itens.isEmpty()) {
+                itens.removeIf(item -> item.getNome().equalsIgnoreCase(nome));
+            }
+        else{
+            System.out.println("Carrinho está vazio");
+        }
+    }
+
+    public double calcularValorTotal(){
+        double valorTotalCarrinho = 0;
+        for (Item item : itens) {
+            valorTotalCarrinho += item.getPreco()*item.getQuantidade();
+        }
+        return valorTotalCarrinho;
+    }
+    
+    public List<Item> exibirItens(){
+        return itens;
+    }
 }
--- List/OperacoesBasicas/src/ProcessoCompra.java
+++ List/OperacoesBasicas/src/ProcessoCompra.java
@@ -1,6 +1,17 @@
 public class ProcessoCompra {
     public static void main(String[] args) {
-        System.out.println("teste");
 
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+
+        carrinho.adicionarItem("Item 1", 10, 2);
+        carrinho.adicionarItem("Item 2", 5, 2);
+        carrinho.adicionarItem("Item 3", 20, 1);
+        carrinho.adicionarItem("Item 4", 30, 1);
+
+        System.out.println(carrinho.exibirItens());
+        System.out.println("o valor atual do carrinho é " + carrinho.calcularValorTotal());
+        carrinho.removerItemPorNome("Item 1");
+        System.out.println(carrinho.exibirItens());
+        System.out.println("o valor atual do carrinho é " + carrinho.calcularValorTotal());
     }
 }
